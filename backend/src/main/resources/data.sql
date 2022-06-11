90a12df91f5e34c2e1294ef37e868c0f4c5d3cb7
MatheusDario <dario.matheus97@gmail.com>
2022-06-10 23:04:27 -0300

    Notification

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -18,3 +18,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T10:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T10:00:00Z', 1);
 
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Ficou muito bom!', TIMESTAMP WITH TIME ZONE '2020-11-25T11:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Ficou bom!', TIMESTAMP WITH TIME ZONE '2020-11-25T11:00:00Z',  true, '/offers/1/resource/1/sections/1', 1);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Precisa melhorar!', TIMESTAMP WITH TIME ZONE '2020-11-25T11:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
+
