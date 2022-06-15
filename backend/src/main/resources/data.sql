189c0c79087766bbe33c9e992c5b44a3469153b4
MatheusDario <dario.matheus97@gmail.com>
2022-06-15 12:26:22 -0300

    Deliver seeding BD

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -48,5 +48,7 @@ INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, du
 INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
 INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
 
+INSERT INTO tb_deliver (uri, moment, status, feedback, correct_Count, user_id, offer_id, lesson_id) VALUES ('https://github.com/MatheusDario/Ds-learn--Bootcamp', TIMESTAMP WITH TIME ZONE '2020-11-28T11:00:00Z', 1, 'Ficou muito bom!', 5, 1, 1, 4);
+
 
 
